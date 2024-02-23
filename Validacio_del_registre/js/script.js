9852de310ff7f5d877aa8833884e40bf4be77e42
jolichh <113618956+jolichh@users.noreply.github.com>
2024-02-23 16:25:16 +0100

    cambiando metodo de codigo

--- Validacio_del_registre/js/script.js
+++ Validacio_del_registre/js/script.js
@@ -1,7 +1,9 @@
 var form = document.querySelector('#form');
 var correo = document.querySelector('#correo');
+var passwd = document.querySelector('#contrasena');
 console.log(form);
 console.log(correo);
+console.log(passwd);
 
 function setGreenBorder(targetElement) {
     targetElement.style.border = "2px solid green";
@@ -10,8 +12,15 @@ function setRedBorder(targetElement) {
     targetElement.style.border = "2px solid red";
 }
 
-form.addEventListener("focusout", function() {
+form.addEventListener("focusout", function(event) {
     //comprobar si esta vacío
+    if (correo == event.target) {
+        if (validateEmail(event.target.value)) {
+            setGreenBorder(event.target);
+        } else {
+            setRedBorder(event.target);
+        }
+    } else
     if(event.target.value.trim() == "" && correo !== event.target) {
         console.log("red from form");
         setRedBorder(event.target);
@@ -27,17 +36,14 @@ form.addEventListener("focusout", function() {
     
 });
 
-function validateEmail(email) {
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
-        console.log("es truee");
-        return true;
-    } else {
-        return false;
-    }
-}
-
-correo.addEventListener("focusout", function(){
-    var esValido = validateEmail(event.target.value);
+//Problema que no marca correo como focusout
+//correo.addEventListener("focusout", function())
+correo.addEventListener("input", function(event){
+    var valorInput = event.target.value.trim();
+    // if (valorInput == "") {
+    //     setRedBorder(event.target);
+    // }
+    var esValido = validateEmail(valorInput);
     
     if (esValido) {
         setGreenBorder(event.target);
@@ -45,3 +51,76 @@ correo.addEventListener("focusout", function(){
         setRedBorder(event.target);
     }
 });
+
+function validateEmail(email) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+        return true;
+    } else {
+        console.log("false from validating email");
+        return false;
+    }
+}
+passwd.addEventListener("input", function(event){
+    if (cumpleCondicion(event.target.value)) {
+        setGreenBorder(event.target);
+    } else {
+        setRedBorder(event.target);
+    }
+});
+//condiciones que debe cumplir para considerarse valido
+function cumpleCondicion(contrasenya) {
+    let lowerCaseLetters = /[a-z]/;
+    let upperCaseLetters = /[A-Z]/;
+    let numbers = /[0-9]/;
+    let specialChars =  /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    var cumpleTodas = false;
+console.log("contraseña itroducida: "+contrasenya);
+    if (contrasenya.length<8 || contrasenya.length>15 ) {
+        console.log("Debe contener entre 8 y 15 caracteres");
+        //return false;
+    }
+    if (!lowerCaseLetters.test(contrasenya)) {
+        console.log("Debe contener 1 letra minúscula");
+        //return false;
+    }
+    if (!upperCaseLetters.test(contrasenya)) {
+        console.log("Debe contener 1 letra mayúscula");
+        //return false;
+    }
+    if (!numbers.test(contrasenya)) {
+        console.log("Debe contener 1 número");
+        //return false;
+    }
+    if (!specialChars.test(contrasenya)) {
+        console.log("Debe contener 1 carácter especial");
+        //return false;
+    }
+    return false;
+}
+
+// document.getElementById("validationPassword").addEventListener("input");
+
+document.getElementById("validationPassword").addEventListener("click",function(event){crearDivCondicionesContrasena(event.target)})
+function crearDivCondicionesContrasena(target) {
+    // var nuevoDiv = document.createElement("div");
+    // nuevoDiv.id = "validationPassword";
+
+    var arrayText = ["Entre 8 y 15 char", "1 mayuscula", "1 numero", "1 caracter especial", "1 minuscula"];
+
+    var arrayID = ["entre", "mayuscula", "numero", "especial", "minuscula"];
+
+    // Crear 5 elementos p y agregarlos al nuevo div con IDs basados en los criterios
+    for (var i = 0; i < criterios.length; i++) {
+        var nuevoP = document.createElement("p");
+        
+        nuevoP.textContent = arrayText[i];       
+        nuevoP.id = arrayID[i];
+        
+        target.appendChild(nuevoP);
+    }
+    // // Obtener el cuerpo del documento
+    // var cuerpo = document.body;
+
+    // // Agregar el nuevo div al cuerpo del documento
+    // cuerpo.appendChild(nuevoDiv);
+}
