fe19f83e2ad91560ce3bff9b984470652aef83a1
jolichh <113618956+jolichh@users.noreply.github.com>
2024-03-31 19:31:17 +0200

    muestra distritos

--- Registre/datosDistritos.php
+++ Registre/datosDistritos.php
@@ -2,19 +2,27 @@
     include('db_connection.php');
 
     $query_multiple = "SELECT * from `districtes`";
+    $distritos = array();
+    
     if (mysqli_multi_query($connexio, $query_multiple)) {
-        echo '<h1>DADES DISTRICTES</h1>';
+        //echo '<h1>DADES DISTRICTES</h1>';
         do {
             //primero guardamos resultado query
             if ($resultado = mysqli_store_result($connexio)) {
                 //mostramos por cada fila de resultado
                 while ($row = mysqli_fetch_array($resultado)) 
                 {
-            echo'Id: '.$row["id"].' '.$row["name"].'<br>';
+            //echo'Id: '.$row["id"].' '.$row["name"].'<br>';
+                    $object = new stdClass();
+                    $object->id = $row["id"];
+                    $object->name = $row["name"];
+                    $distritos[] = $object; 
                 }
-                mysqli_free_result($resultado);
+                //mysqli_free_result($resultado);
             }                    
         } while (mysqli_next_result($connexio));    
-        //mientras haya resultado hace bucle
     }
+
+    // Devolver los distritos en formato JSON
+    echo json_encode($distritos);
 ?>
--- Registre/form_registrePisos.php
+++ Registre/form_registrePisos.php
@@ -6,7 +6,8 @@
 	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
 	<link rel="stylesheet" href="css/style.css">	
 </head>
 
@@ -80,7 +81,7 @@
 
 						<div class="col-4">
 							<label for="">Districte</label>
-							<select class="custom-select">
+							<select id="selectDistricte" class="custom-select">
 								<option selected>Open this select menu</option>
 								<option value="1">One</option>
 								<option value="2">Two</option>
@@ -90,7 +91,7 @@
 
 						<div class="col-4">
 							<label for="">Barri</label>
-							<select class="custom-select">
+							<select id="selectBarri" class="custom-select">
 								<option selected>Open this select menu</option>
 								<option value="1">One</option>
 								<option value="2">Two</option>
